5773982f61cdf9a5ec98f2835db0de77fc865b01
LG <92387298+LGnan@users.noreply.github.com>
2023-11-21 15:22:50 -0600

    Ultimo commit

--- decrypt.py
+++ decrypt.py
@@ -1,14 +1,16 @@
-# Solicitamos la clave al usuario
-numero = int(input("Por favor, ingresa el número (clave): "))
+# Validamos que el número ingresado sea un entero
+while True:
+    try:
+        numero = int(input("Por favor, ingresa el número (clave): "))
+        break
+    except ValueError:
+        print("Por favor, ingresa un número entero.")
 
-# Leemos la imagen encriptada y la convertimos a bytes
-with open("imagen_encriptada.jpg", "rb") as file:
+with open("encrypt.jpg", "rb") as file:
     image = bytearray(file.read())
 
-# Desencriptamos la imagen
 for i, j in enumerate(image):
     image[i] = j ^ numero
 
-# Guardamos la imagen desencriptada
 with open("decrypted.jpg", "wb") as file:
     file.write(image)
--- encrypt.py
+++ encrypt.py
@@ -1,25 +1,26 @@
 import os
 
-# Solicitamos la ruta de la imagen y la clave al usuario
 ruta_imagen = input("Por favor, ingresa la ruta de la imagen: ")
 extension = os.path.splitext(ruta_imagen)[1].upper()
 
-# Validamos la extensión de la imagen
 while extension not in [".JPEG", ".PNG", ".JPG"]:
     print("Por favor, ingresa una imagen con extensión .JPEG, .PNG o .JPG.")
     ruta_imagen = input("Por favor, ingresa la ruta de la imagen: ")
     extension = os.path.splitext(ruta_imagen)[1].upper()
 
-numero = int(input("Por favor, ingresa un número(clave): "))
+# Validamos que el número ingresado sea un entero
+while True:
+    try:
+        numero = int(input("Por favor, ingresa un número(clave): "))
+        break
+    except ValueError:
+        print("Por favor, ingresa un número entero.")
 
-# Leemos la imagen y la convertimos a bytes
 with open(ruta_imagen, "rb") as file:
     image = bytearray(file.read())
 
-# Encriptamos la imagen
 for i, j in enumerate(image):
     image[i] = j ^ numero
 
-# Guardamos la imagen encriptada
-with open("imagen_encriptada.jpg", "wb") as file:
+with open("encrypt.jpg", "wb") as file:
     file.write(image)
