452dbd7303e8b74ece8ab9d01c275833b014517d
LG <92387298+LGnan@users.noreply.github.com>
2023-11-21 14:49:28 -0600

    Avances para que el profe vea aún no acabop

--- decrypt.py
+++ decrypt.py
@@ -1,25 +1,26 @@
 from cryptography.fernet import Fernet
 from PIL import Image
 import io
+import hashlib
+import base64
 
-# Solicitamos al usuario que ingrese la ruta donde guardar la imagen
+# Solicitamos al usuario que ingrese la ruta donde guardar la imagen y el número
 ruta_imagen = input("Por favor, ingresa la ruta donde guardar la imagen: ")
+numero = input("Por favor, ingresa el número: ")
 
-# Leemos la imagen encriptada y la clave
-with open('imagen_encriptada', 'rb') as f:
-    imagen_encriptada = f.read()
-with open('clave', 'rb') as f:
-    clave = f.read()
-
-# Creamos el objeto de cifrado
+# Generamos la clave de encriptación a partir del número
+clave = base64.urlsafe_b64encode(hashlib.sha256(numero.encode()).digest())
 cifrado = Fernet(clave)
+
+# Leemos la imagen encriptada
+with open("imagen_encriptada", "rb") as f:
+    imagen_encriptada = f.read()
 
 # Desencriptamos la imagen
 imagen_desencriptada = cifrado.decrypt(imagen_encriptada)
 
 # Convertimos los bytes a imagen
 imagen_final = Image.open(io.BytesIO(imagen_desencriptada))
-imagen_final.save(ruta_imagen, 'JPEG')  # Guardamos la imagen en formato JPEG
 
-#pip3 install cryptography
-#pip3 install Pillow
+# Guardamos la imagen en el formato JPEG
+imagen_final.save(ruta_imagen, "JPEG")
--- encrypt.py
+++ encrypt.py
@@ -1,26 +1,27 @@
-from cryptography.fernet import Fernet
-import base64
-from PIL import Image
-import io
-import hashlib
+# Importamos las bibliotecas necesarias
+from cryptography.fernet import Fernet  # Biblioteca para cifrado simétrico
+import base64  # Para codificación y decodificación en base64
+from PIL import Image  # Biblioteca para manipulación de imágenes
+import io  # Para trabajar con streams de bytes
+import hashlib  # Para generar hash de la clave
 
 # Solicitamos al usuario que ingrese la ruta de la imagen y el número
 ruta_imagen = input("Por favor, ingresa la ruta de la imagen: ")
 numero = input("Por favor, ingresa un número: ")
 
 # Generamos una clave de encriptación a partir del número
-clave = base64.urlsafe_b64encode(hashlib.md5(numero.encode()).digest())
-cifrado = Fernet(clave)
+clave = base64.urlsafe_b64encode(hashlib.sha256(numero.encode()).digest())
+cifrado = Fernet(clave)  # Creamos un objeto Fernet con la clave generada
 
 # Abrimos la imagen y la convertimos a bytes
 imagen = Image.open(ruta_imagen)
 byte_arr = io.BytesIO()
-imagen.save(byte_arr, format='JPEG')  # Cambiamos el formato a JPEG
+imagen.save(byte_arr, format="JPEG")  # Guardamos la imagen en un stream de bytes en formato JPEG
 imagen_en_bytes = byte_arr.getvalue()
 
-# Encriptamos la imagen
+# Encriptamos la imagen utilizando la clave generada
 imagen_encriptada = cifrado.encrypt(imagen_en_bytes)
 
-# Guardamos la imagen encriptada
-with open('imagen_encriptada', 'wb') as f:
+# Guardamos la imagen encriptada en un archivo llamado "imagen_encriptada"
+with open("imagen_encriptada", "wb") as f:
     f.write(imagen_encriptada)
